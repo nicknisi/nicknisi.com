138cdbca6ea7919d6c180f686854cc3596d5de5e
Nick Nisi <nick@nisi.org>
2020-08-25 22:25:50 -0500

    add better support for svg embeds
    allow a `classes` argument to be passed to the embed plugin

--- _11ty/embed.js
+++ _11ty/embed.js
@@ -1,6 +1,7 @@
 const { readFileSync } = require('fs');
+const cheerio = require('cheerio');
 
-module.exports = (filePath) => {
+module.exports = (filePath, classes) => {
     const data = readFileSync(`.${filePath}`, (error, data) => {
         if (error) {
             throw new Error(error);
@@ -9,5 +10,11 @@ module.exports = (filePath) => {
         return data.toString('utf8');
     });
 
-    return data;
+    const $ = cheerio.load(data, { xmlMode: true });
+
+    if (classes) {
+        $('svg').addClass(classes);
+    }
+
+    return $.html('svg');
 };
