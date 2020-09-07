9cb40f989757315d5b04466dda72b3fdf86c084c
Nick Nisi <nick@nisi.org>
2020-09-07 14:20:41 -0500

    add cool-gray color config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,20 @@
 module.exports = {
     purge: ['_includes/**/*.njk', 'img/**/*.svg'],
-    theme: {},
+    theme: {
+        extend: {
+            colors: {
+                'cool-gray': {
+                    100: '#E4E7EB',
+                    200: '#CBD2D9',
+                    300: '#9AA5B1',
+                    400: '#7B8794',
+                    500: '#616E7C',
+                    600: '#52606D',
+                    700: '#3E4C59',
+                    800: '#323F4B',
+                    900: '#1F2933',
+                },
+            },
+        },
+    },
 };
