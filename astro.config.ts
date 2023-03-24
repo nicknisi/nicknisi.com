6a949547c37805124587760d4d2dfabdf74bd8ac
Nick Nisi <nicknisi@hephaestus.local>
2023-03-24 11:40:43 -0500

    set up astro Image

--- astro.config.ts
+++ astro.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import image from '@astrojs/image';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,13 +15,21 @@ export default defineConfig({
   server: {
     port: 8080,
   },
-  integrations: [react(), mdx(), tailwind(), sitemap({
-    filter: (page) => page !== 'https://nicknisi.com/resume',
-  })],
+  integrations: [
+    react(),
+    mdx(),
+    tailwind(),
+    image({
+      serviceEntryPoint: '@astrojs/image/sharp',
+    }),
+    sitemap({
+      filter: (page) => page !== 'https://nicknisi.com/resume',
+    }),
+  ],
   markdown: {
     shikiConfig: {
       theme: 'dracula',
-      wrap: true
-    }
-  }
+      wrap: true,
+    },
+  },
 });
