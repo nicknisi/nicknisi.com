4146bb08169af629d3ad7e86549c473880ce2bb8
Nick Nisi <nick@nisi.org>
2024-02-02 13:39:38 -0600

    update talks page to support pages

---
+++ src/types.ts
@@ -0,0 +1,14 @@
+export type TalkSource = 'youtube' | 'vimeo' | 'page' | 'website';
+export type TalkType = 'talk' | 'workshop' | 'panel' | 'keynote' | 'interview' | 'other';
+
+export interface Talk {
+	title: string;
+	date: string;
+	location: string;
+	url: string;
+	videoId: string;
+	promote?: boolean;
+	source: TalkSource;
+	type: TalkType;
+	remote?: boolean;
+}
--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,6 @@
 import { DateTime } from 'luxon';
 import { getCollection } from 'astro:content';
+import { Talk } from '@/types.js';
 
 /**
  * Convert a data to a readable format
@@ -7,10 +8,10 @@ import { getCollection } from 'astro:content';
  * @returns Readable date string
  */
 export function readableDate(dateObj: Date | string) {
-  if (typeof dateObj === 'string') {
-    dateObj = new Date(dateObj);
-  }
-  return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('dd LLL yyyy');
+	if (typeof dateObj === 'string') {
+		dateObj = new Date(dateObj);
+	}
+	return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('dd LLL yyyy');
 }
 
 /**
@@ -18,26 +19,43 @@ export function readableDate(dateObj: Date | string) {
  * @param content - Content of the blog post
  */
 export function getWordCount(content: string = '') {
-  const length = content.split(/\s+/gu).length;
-  return Math.ceil(length / 240);
+	const length = content.split(/\s+/gu).length;
+	return Math.ceil(length / 240);
 }
 
 export async function getPosts(max?: number) {
-  const mode = import.meta.env.MODE;
-  const posts = (
-    await getCollection('posts', (post) => {
-      if (mode === 'production') {
-        return !post.data.draft;
-      }
-      return true;
-    })
-  ).sort((a, b) => {
-    return new Date(a.data.pubDate) > new Date(b.data.pubDate) ? -1 : 1;
-  });
+	const mode = import.meta.env.MODE;
+	const posts = (
+		await getCollection('posts', post => {
+			if (mode === 'production') {
+				return !post.data.draft;
+			}
+			return true;
+		})
+	).sort((a, b) => {
+		return new Date(a.data.pubDate) > new Date(b.data.pubDate) ? -1 : 1;
+	});
 
-  if (max) {
-    return posts.slice(0, max);
-  }
+	if (max) {
+		return posts.slice(0, max);
+	}
 
-  return posts;
+	return posts;
+}
+
+export async function getThumbnailUrl(talk: Talk) {
+	if (talk.source === 'vimeo') {
+		const response = await fetch(`http://vimeo.com/api/v2/video/${talk.videoId}.json`);
+		if (response.ok) {
+			const data = await response.json();
+			console.log('RESPONSE', data);
+			return data[0].thumbnail_medium as string;
+		}
+	}
+
+	if (talk.videoId) {
+		return `https://img.youtube.com/vi/${talk.videoId}/hqdefault.jpg`;
+	}
+
+	return '/img/talk_thumbnail.png';
 }
