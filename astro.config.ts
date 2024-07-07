ddd3745df209b349f0c6d616616545d51106fee1
Nick Nisi <nick@nisi.org>
2024-07-07 12:16:37 -0500

    move redirects into astro config

--- astro.config.ts
+++ astro.config.ts
@@ -33,4 +33,18 @@ export default defineConfig({
 			wrap: true,
 		},
 	},
+	redirects: {
+		// redirects for mastodon
+		'/.well-known/host-meta*': 'https://fediverse.nicknisi.com/.well-known/host-meta:splat',
+		'/.well-known/webfinger*': 'https://fediverse.nicknisi.com/.well-known/webfinger:splat',
+		'/.well-known/nodeinfo*': 'https://fediverse.nicknisi.com/.well-known/nodeinfo:splat',
+		'/@nicknisi': 'https://fediverse.nicknisi.com/@nicknisi',
+
+		// redirects for RSS feed
+		'/feed/feed.xml': 'https://nicknisi.com/rss.xml',
+
+		// simpler talk redirects for sharing
+		'/state-talk': '/talks/componentizing-application-state',
+		'/compiler-talk': '/talks/unleashing-the-typescript-compiler',
+	},
 });
