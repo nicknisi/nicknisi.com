3b932ed583f8fc159bfccf6d19443c38de204e38
Nick Nisi <nick@nisi.org>
2024-11-27 11:27:11 -0600

    refactor Callout component to be more stylish

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,16 +17,17 @@ const config: Config = {
 			}),
 		},
 		fontSize: {
-			xs: '.65rem',
-			sm: '.75rem',
-			base: '.875rem',
-			ls: '1rem',
-			xl: '1.125rem',
-			'2xl': '1.25rem',
-			'3xl': '1.5rem',
-			'4xl': '1.875rem',
-			'5xl': '2.25rem',
-			'6xl': '3rem',
+			xs: '0.75rem', // 12px
+			sm: '0.875rem', // 14px
+			base: '1rem', // 16px
+			lg: '1.125rem', // 18px
+			xl: '1.25rem', // 20px
+			'2xl': '1.5rem', // 24px
+			'3xl': '1.875rem', // 30px
+			'4xl': '2.25rem', // 36px
+			'5xl': '3rem', // 48px
+			'6xl': '3.75rem', // 60px
+			'7xl': '4.5rem', // 72px
 		},
 		fontFamily: {
 			sans: ['Atkinson Hyperlegible', 'sans-serif'],
