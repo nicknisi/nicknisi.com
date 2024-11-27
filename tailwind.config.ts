75e9044e44d4bb0536a3070e2f3d2a857c3aaca5
Nick Nisi <nick@nisi.org>
2024-11-27 06:03:30 -0600

    feat: add theme toggle component

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import { type Config } from 'tailwindcss';
 const config: Config = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	safeList: [],
-	// darkMode: 'class',
+	darkMode: 'class',
 	theme: {
 		extend: {
 			typography: () => ({
