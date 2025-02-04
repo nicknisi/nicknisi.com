13e49a6ca2b6e476c8893154c233443c5fcc9f00
Nick Nisi <nick@nisi.org>
2025-02-03 21:09:57 -0600

    fix rss

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -4,20 +4,22 @@ import { getCollection } from 'astro:content';
 import sanitizeHtml from 'sanitize-html';
 import markdownIt from 'markdown-it';
 
-export async function get({ site }: { site: string }) {
+export async function GET({ site }: { site: URL }) {
 	const posts = (await getCollection('posts', post => !post.data.draft)).reverse();
 	return rss({
 		title: metadata.title,
 		description: metadata.description,
-		site,
+		site: site.toString(),
 		customData: `<language>en-us</language>`,
-		items: posts.map(post => ({
-			...post.data,
-			title: post.data.title,
-			description: post.data.description,
-			pubDate: post.data.pubDate,
-			link: `/posts/${post.slug}`,
-			content: sanitizeHtml(markdownIt().render(post.body)),
-		})),
+		items: posts
+			.filter(post => !post.data.external)
+			.map(post => ({
+				...post.data,
+				title: post.data.title,
+				description: post.data.description,
+				pubDate: post.data.pubDate,
+				link: `/posts/${post.slug}`,
+				content: sanitizeHtml(markdownIt().render(post.body)),
+			})),
 	});
 }
